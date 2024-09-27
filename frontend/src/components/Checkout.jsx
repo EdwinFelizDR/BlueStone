6aa74561a618d7df99b7301d9d1967e38eadbe12
EdwinFelizDR <efeliz12lg@gmail.com>
2024-09-27 16:08:49 -0400

    working in checkout page

--- frontend/src/components/Checkout.jsx
+++ frontend/src/components/Checkout.jsx
@@ -1,153 +1,261 @@
 import React from "react";
+import "../css/Checkout.css";
+
 
 function Checkout() {
   return (
-    <>
-      <main id="main" role="main">
-        <section id="checkout-container">
+    <main id="main" role="main" className="container">
+      <section id="checkout-container" className="section">
+        <div>
           <div>
             <div>
-              <div>
-                <h4>Billing address</h4>
-                <form noValidate>
-                  <div>
-                    <div>
-                      <label htmlFor="firstName">First name</label>
-                      <input type="text" id="firstName" placeholder="" required/>
-                      <div>Valid first name is required.</div>
-                    </div>
-                    <div>
-                      <label htmlFor="lastName">Last name</label>
-                      <input type="text" id="lastName" placeholder="" required />
-                      <div>Valid last name is required.</div>
-                    </div>
-                  </div>
-
-                  <div>
-                    <label htmlFor="username">Username</label>
-                    <div>
-                      <div>
-                        <span>@</span>
-                      </div>
-                      <input type="text" id="username" placeholder="Username" required />
-                      <div>Your username is required.</div>
-                    </div>
-                  </div>
-
-                  <div>
-                    <label htmlFor="email">
-                      Email <span>(Optional)</span>
+              <h4>Billing address</h4>
+              <form noValidate>
+                <div className="form-group">
+                  <div className="form-group">
+                    <label htmlFor="firstName" className="label">
+                      First name
                     </label>
-                    <input type="email" id="email" placeholder="you@example.com"/>
-                    <div>
-                      Please enter a valid email address for shipping updates.
+                    <input
+                      type="text"
+                      id="firstName"
+                      placeholder=""
+                      required
+                      className="input"
+                    />
+                    <div className="error-message">
+                      Valid first name is required.
                     </div>
                   </div>
-
-                  <div>
-                    <label htmlFor="address">Address</label>
-                    <input type="text" id="address" placeholder="1234 Main St" required />
-                    <div>Please enter your shipping address.</div>
-                  </div>
-
-                  <div>
-                    <label htmlFor="address2">
-                      Address 2 <span>(Optional)</span>
+                  <div className="form-group">
+                    <label htmlFor="lastName" className="label">
+                      Last name
                     </label>
-                    <input type="text" id="address2" placeholder="Apartment or suite"/>
+                    <input
+                      type="text"
+                      id="lastName"
+                      placeholder=""
+                      required
+                      className="input"
+                    />
+                    <div className="error-message">
+                      Valid last name is required.
+                    </div>
                   </div>
+                </div>
 
-                  <div>
+                <div className="form-group">
+                  <label htmlFor="username" className="label">
+                    Username
+                  </label>
+                  <div className="form-group">
                     <div>
-                      <label htmlFor="country">Country</label>
-                      <select id="country" required>
-                        <option value="">Choose...</option>
-                        <option>United States</option>
-                      </select>
-                      <div>Please select a valid country.</div>
+                      <span>@</span>
                     </div>
-                    <div>
-                      <label htmlFor="state">State</label>
-                      <select id="state" required>
-                        <option value="">Choose...</option>
-                        <option>California</option>
-                      </select>
-                      <div>Please provide a valid state.</div>
-                    </div>
-                    <div>
-                      <label htmlFor="zip">Zip</label>
-                      <input type="text" id="zip" placeholder="" required />
-                      <div>Zip code required.</div>
-                    </div>
+                    <input
+                      type="text"
+                      id="username"
+                      placeholder="Username"
+                      required
+                      className="input"
+                    />
+                    <div className="error-message">Your username is required.</div>
                   </div>
-                  <hr />
-                  <div>
-                    <input type="checkbox" id="same-address" />
-                    <label htmlFor="same-address">
-                      Shipping address is the same as my billing address
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="email" className="label">
+                    Email <span>(Optional)</span>
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="you@example.com"
+                    className="input"
+                  />
+                  <div className="error-message">
+                    Please enter a valid email address for shipping updates.
+                  </div>
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="address" className="label">
+                    Address
+                  </label>
+                  <input
+                    type="text"
+                    id="address"
+                    placeholder="1234 Main St"
+                    required
+                    className="input"
+                  />
+                  <div className="error-message">
+                    Please enter your shipping address.
+                  </div>
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="address2" className="label">
+                    Address 2 <span>(Optional)</span>
+                  </label>
+                  <input
+                    type="text"
+                    id="address2"
+                    placeholder="Apartment or suite"
+                    className="input"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <div className="form-group">
+                    <label htmlFor="country" className="label">
+                      Country
                     </label>
+                    <select id="country" required className="input">
+                      <option value="">Choose...</option>
+                      <option>United States</option>
+                    </select>
+                    <div className="error-message">
+                      Please select a valid country.
+                    </div>
                   </div>
-                  <div>
-                    <input type="checkbox" id="save-info" />
-                    <label htmlFor="save-info">
-                      Save this information for next time
+                  <div className="form-group">
+                    <label htmlFor="state" className="label">
+                      State
                     </label>
+                    <select id="state" required className="input">
+                      <option value="">Choose...</option>
+                      <option>California</option>
+                    </select>
+                    <div className="error-message">
+                      Please provide a valid state.
+                    </div>
                   </div>
-                  <hr />
+                  <div className="form-group">
+                    <label htmlFor="zip" className="label">
+                      Zip
+                    </label>
+                    <input
+                      type="text"
+                      id="zip"
+                      placeholder=""
+                      required
+                      className="input"
+                    />
+                    <div className="error-message">Zip code required.</div>
+                  </div>
+                </div>
+                <hr className="hr" />
+                <div className="form-group">
+                  <input type="checkbox" id="same-address" />
+                  <label htmlFor="same-address">
+                    Shipping address is the same as my billing address
+                  </label>
+                </div>
+                <div className="form-group">
+                  <input type="checkbox" id="save-info" />
+                  <label htmlFor="save-info">
+                    Save this information for next time
+                  </label>
+                </div>
+                <hr className="hr" />
 
-                  <h4>Payment</h4>
+                <h4>Payment</h4>
 
-                  <div>
-                    <div>
-                      <input id="credit" name="paymentMethod" type="radio" defaultChecked required/>
-                      <label htmlFor="credit">Credit card</label>
-                    </div>
-                    <div>
-                      <input id="debit" name="paymentMethod" type="radio" required/>
-                      <label htmlFor="debit">Debit card</label>
-                    </div>
-                    <div>
-                      <input id="paypal" name="paymentMethod" type="radio" required/>
-                      <label htmlFor="paypal">Paypal</label>
+                <div className="form-group">
+                  <div className="form-group">
+                    <input
+                      id="credit"
+                      name="paymentMethod"
+                      type="radio"
+                      defaultChecked
+                      required
+                    />
+                    <label htmlFor="credit">Credit card</label>
+                  </div>
+                  <div className="form-group">
+                    <input id="debit" name="paymentMethod" type="radio" required />
+                    <label htmlFor="debit">Debit card</label>
+                  </div>
+                  <div className="form-group">
+                    <input id="paypal" name="paymentMethod" type="radio" required />
+                    <label htmlFor="paypal">Paypal</label>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <div className="form-group">
+                    <label htmlFor="cc-name" className="label">
+                      Name on card
+                    </label>
+                    <input
+                      type="text"
+                      id="cc-name"
+                      placeholder=""
+                      required
+                      className="input"
+                    />
+                    <small className="small-text">
+                      Full name as displayed on card
+                    </small>
+                    <div className="error-message">Name on card is required</div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="cc-number" className="label">
+                      Credit card number
+                    </label>
+                    <input
+                      type="text"
+                      id="cc-number"
+                      placeholder=""
+                      required
+                      className="input"
+                    />
+                    <div className="error-message">
+                      Credit card number is required
                     </div>
                   </div>
-                  <div>
-                    <div>
-                      <label htmlFor="cc-name">Name on card</label>
-                      <input type="text" id="cc-name" placeholder="" required />
-                      <small>Full name as displayed on card</small>
-                      <div>Name on card is required</div>
-                    </div>
-                    <div>
-                      <label htmlFor="cc-number">Credit card number</label>
-                      <input type="text" id="cc-number" placeholder="" required/>
-                      <div>Credit card number is required</div>
-                    </div>
+                </div>
+                <div className="form-group">
+                  <div className="form-group">
+                    <label htmlFor="cc-expiration" className="label">
+                      Expiration
+                    </label>
+                    <input
+                      type="text"
+                      id="cc-expiration"
+                      placeholder=""
+                      required
+                      className="input"
+                    />
+                    <div className="error-message">Expiration date required</div>
                   </div>
-                  <div>
-                    <div>
-                      <label htmlFor="cc-expiration">Expiration</label>
-                      <input type="text" id="cc-expiration" placeholder="" required/>
-                      <div>Expiration date required</div>
-                    </div>
-                    <div>
-                      <label htmlFor="cc-cvv">CVV</label>
-                      <input type="text" id="cc-cvv" placeholder="" required />
-                      <div>Security code required</div>
-                    </div>
+                  <div className="form-group">
+                    <label htmlFor="cc-cvv" className="label">
+                      CVV
+                    </label>
+                    <input
+                      type="text"
+                      id="cc-cvv"
+                      placeholder=""
+                      required
+                      className="input"
+                    />
+                    <div className="error-message">Security code required</div>
                   </div>
-                  <hr />
-                  <button type="submit">Place order</button>
-                </form>
-              </div>
+                </div>
+                <hr className="hr" />
+                <button type="submit" className="button">
+                  Place order
+                </button>
+              </form>
             </div>
           </div>
-          <a href="#">
-            <i></i>
-          </a>
-        </section>
-      </main>
-    </>
+        </div>
+        <a href="#">
+          <i></i>
+        </a>
+      </section>
+    </main>
   );
 }
 
